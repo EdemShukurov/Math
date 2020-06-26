bf0c13a9b04251f735f82871870df53d11d966ff
Edem Shukurov <43962752+EdemShukurov@users.noreply.github.com>
2020-06-26 21:13:08 +0500

    Add .gitignore and TestDimArray class
    TestDimArray is needed for test unsafe code

---
+++ TestDimArray.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Math
+{
+    public static class TestDimArray
+    {
+        public static int SumOfElements(int[,] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
+                {
+                    sum += array[i, j];
+                }
+            }
+
+            return sum;
+        }
+
+        public static int SumOfElements(int[][] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
+                {
+                    sum += array[i][j];
+                }
+            }
+
+            return sum;
+        }
+
+        public static unsafe int UnsafeSumOfElements(int[,] array)
+        {
+            int sum = 0;
+
+            fixed (int* pi = array)
+            {
+                for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+                {
+                    int baseOfDim = i * (array.GetUpperBound(0) + 1);
+
+                    for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
+                    {
+                        sum += pi[baseOfDim + j];
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
